d7f7d4218d7ef356934de4fc3505f13cfefeede7
Tommy <tngo@pdx.edu>
2021-05-22 19:15:27 -0700

    Changed card cs issue

--- Client-Ui/src/Components/About.js
+++ Client-Ui/src/Components/About.js
@@ -6,21 +6,21 @@ const About = () => {
   return (
     <>
       <Container className="aboutUs">
-        <Card className="card" style={{ width: "25rem" }}>
+        <Card className="about-us-card" style={{ width: "25rem" }}>
           <Card.Img className="img" variant="top" src="holder.js/100px180" />
           <Card.Body>
             <Card.Title className="title">Mackenzie Gage</Card.Title>
             <Card.Text>Some quick example t</Card.Text>
           </Card.Body>
         </Card>
-        <Card className="card" style={{ width: "25rem" }}>
+        <Card className="about-us-card" style={{ width: "25rem" }}>
           <Card.Img className="img" variant="top" src="holder.js/100px180" />
           <Card.Body>
             <Card.Title className="title">Tommy Ngo</Card.Title>
             <Card.Text>Some quick exampu</Card.Text>
           </Card.Body>
         </Card>
-        <Card className="card" style={{ width: "25rem" }}>
+        <Card className="about-us-card" style={{ width: "25rem" }}>
           <Card.Img className="img" variant="top" src={Rio} />
           <Card.Body>
             <Card.Title className="title">Rio Le</Card.Title>
